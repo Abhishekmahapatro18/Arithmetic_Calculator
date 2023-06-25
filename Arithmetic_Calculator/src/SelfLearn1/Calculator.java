3abd675a26af6db0cab8104e2096a53e6e6fec31
Abhishekmahapatro18 <137686541+Abhishekmahapatro18@users.noreply.github.com>
2023-06-25 20:16:18 +0530

    V1.2
    Added this key word

--- Arithmetic_Calculator/src/SelfLearn1/Calculator.java
+++ Arithmetic_Calculator/src/SelfLearn1/Calculator.java
@@ -1,102 +1,54 @@
 package SelfLearn1;
 import java.util.Scanner;
 
-class Arithmetic_Calculator 
+public class Calculator 
 {
-    private double num1;
-    private double num2;
-    
-    public Arithmetic_Calculator(double num1, double num2) 
+    public static void main(String[] args) 
     {
-        this.num1 = num1;
-        this.num2 = num2;
-    }
-    
-    public double add() 
-    {
-        return num1 + num2;
-    }
-    
-    public double subtract() 
-    {
-        return num1 - num2;
-    }
-    
-    public double multiply() 
-    {
-        return num1 * num2;
-    }
-    
-    public double divide() 
-    {
-        if (num2 != 0) 
-        {
-            return num1 / num2;
-        } 
-        else 
-        {
-            throw new ArithmeticException("Error: Cannot divide by zero!");
-        }
-    }
-}
-public class Calculator
-{
-    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Arithmetic Calculator");
-        System.out.println("---------------------");
-        
-        System.out.print("Enter the first number: ");
+        System.out.print("Enter the first number : ");
         double num1 = scanner.nextDouble();
         
-        System.out.print("Enter the second number: ");
+        System.out.print("Enter the second number : ");
         double num2 = scanner.nextDouble();
         
-        Arithmetic_Calculator calculator = new Arithmetic_Calculator(num1, num2);
+        System.out.println("Select the operation (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
         
-        System.out.println("\nSelect an operation:");
-        System.out.println("1. Addition (+)");
-        System.out.println("2. Subtraction (-)");
-        System.out.println("3. Multiplication (*)");
-        System.out.println("4. Division (/)");
+        double result;
         
-        System.out.print("Enter your choice: ");
-        int choice = scanner.nextInt();
-        
-        double result = 0;
-        String operator = "";
-        
-        switch (choice) {
-            case 1:
-                result = calculator.add();
-                operator = "+";
+        switch (operator) 
+        {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
                 break;
-            case 2:
-                result = calculator.subtract();
-                operator = "-";
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
                 break;
-            case 3:
-                result = calculator.multiply();
-                operator = "*";
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
                 break;
-            case 4:
-                try {
-                    result = calculator.divide();
-                    operator = "/";
-                } catch (ArithmeticException e) {
-                    System.out.println(e.getMessage());
-                    return;
+            case '/':
+                if (num2 == 0) 
+                {
+                    System.out.println("Error: Cannot divide by zero!");
+                } 
+                else 
+                {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
                 }
                 break;
             default:
-                System.out.println("Invalid choice!");
-                return;
+                System.out.println("Error: Invalid operator!!!");
         }
         
-        System.out.println("\nResult:");
-        System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
-        
         scanner.close();
+        System.out.println("I am done.");
     }
 }
+
